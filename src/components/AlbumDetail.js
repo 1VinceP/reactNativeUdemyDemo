bb668593840068bce8b9d4c0267a76c379979d6e
Vincent Palmer <vpalmergraphics@gmail.com>
2017-12-04 16:44:02 -0700

    displays album header

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,18 +5,22 @@ import Card from './Card';
 import CardSection from './CardSection'
 
 export default function AlbumDetail( { album } ) {
-    const { title, artist, thumbnail_image } = album
+    const { title, artist, thumbnail_image, image } = album
+    const { headerStyle, headerText, thumbnailStyle, thumbnailContainerStyle, imageStyle } = styles
 
     return (
         <Card>
             <CardSection>
-                <View>
-                    <Image source={{ uri: thumbnail_image}} style={styles.thumbnailStyle} />
+                <View style={thumbnailContainerStyle}>
+                    <Image source={{ uri: thumbnail_image}} style={thumbnailStyle} />
                 </View>
-                <View style={styles.headerStyle}>
-                    <Text>{title}</Text>
+                <View style={headerStyle}>
+                    <Text style={headerText}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                <Image source={{ uri: image }} style={imageStyle} />
             </CardSection>
         </Card>
     )
@@ -27,8 +31,22 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerText: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 }
